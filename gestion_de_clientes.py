5a7ea66fd92509280fbe51b4affc30dd921e78fa
LuisCerelli <cerluisalberto@gmail.com>
2024-09-08 12:49:19 +0200

    agregando funciones

--- gestion_de_clientes.py
+++ gestion_de_clientes.py
@@ -1,18 +1,26 @@
 
-#Creamos la variable y la funcion para cargar a los clientes: 
-
+#Creamos la variable donde cargaremos a los clientes:
 clientes = []
 
-def registrar_cliente(nombre, apellido):
+#y la función para cargar a los clientes: 
+def registrar_cliente(nombre, apellido, rut):#rut sería el numero de cliente... 
     cliente = {
         "nombre": nombre,
-        "apellido": apellido
+        "apellido": apellido,
+        "mascotas": [],
+        "rut":rut
     }
 
     clientes.append(cliente)
+    print(f"Cliente {nombre} {apellido} registrado correctamente.")
 
-print(clientes)    
+#Hacemos una función para agregar la mascota(o lo que sea a un cliente, por ejemplo un auto si es un mecánico o una prenda si es un inventario de clientes, etc)...
 
-registrar_cliente("Luis","Cerelli")
-
-print(clientes)
+def agregar_mascota(rut, nombre, tipo):
+    for cliente in clientes:
+        if cliente["rut"] == rut:
+            mascota = {
+                "nombre": nombre,
+                "tipo": tipo
+            }
+            cliente["mascotas"].append(mascota)
